bf332bd9a85ecb7e20da9d1d7c924c2cfaeab9e1
Zigfruid <Zigfruid@gmail.com>
2020-05-26 20:38:12 +0500

    set up db

---
+++ app/src/main/java/com/example/redbook/data/RedBookDataBase.kt
@@ -0,0 +1,28 @@
+package com.example.redbook.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.redbook.data.dao.AnimalDao
+import com.example.redbook.data.model.Animal
+
+
+@Database(entities = [Animal::class], version = 1)
+abstract  class RedBookDataBase : RoomDatabase() {
+
+    companion object{
+        private lateinit var INSTANCE: RedBookDataBase
+        fun getInstance(context: Context) : RedBookDataBase =
+            Room.databaseBuilder(
+                context,
+                RedBookDataBase::class.java,
+                "book-database.db"
+            )
+                .createFromAsset("book-database.db")
+                .build()
+    }
+
+
+    abstract fun dao(): AnimalDao
+}
